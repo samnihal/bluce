9bf4f774cf4372004ba8c1ca09b28217d82fb0f0
Samnihal <samnihal7892@gmail.com>
2019-05-20 02:30:40 +0530

    this is first change

---
+++ sam2.java
@@ -0,0 +1,22 @@
+bin/
+cmd/
+dev/
+etc/
+git-bash.exe*
+git-cmd.exe*
+LICENSE.txt
+mingw64/
+proc/
+ReleaseNotes.html
+tmp/
+unins000.dat
+unins000.exe*
+unins000.msg
+sr/
+sr/
+this is sam
+heloo my first program
+:WO
+git save
+:insert
+this is second edit
